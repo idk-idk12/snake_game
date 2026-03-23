c9cc8462943799e8eb561600d845e4d2681c3e42
Jonathan C <jonathan1050864@my.yosemite.edu>
2026-03-23 14:56:08 -0700

    Created draw function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,33 @@
+#include <iostream>
 #include <vector>
 #include <string>
 
+
+void draw() {
+	int nrows = 15;
+	int ncols = 60;
+	int x, y;
+	std::cout << "|";
+	for (x = 0; x < ncols; x++) {
+		std::cout << "-";
+	}
+	std::cout << "|" << std::endl;
+	for (x = 0; x < nrows; x++) {
+		std::cout << "|";
+		for (y = 0; y < ncols; y++) {
+			std::cout << " ";
+		}
+		std::cout << "|" << std::endl;
+	}
+	std::cout << "|";
+	for (x = 0; x < ncols; x++) {
+		std::cout << "-";
+	}
+	std::cout << "|" << std::endl;
+}
+
+
 int main() {
+	draw();
 	return 0;
 }
