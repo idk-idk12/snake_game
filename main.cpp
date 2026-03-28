52c63f6f07010e52b46a7953d5f3419749e21885
Jonathan C <jonathan1050864@my.yosemite.edu>
2026-03-27 20:33:57 -0700

    Made the grid with hashtags instead of '-'

--- main.cpp
+++ main.cpp
@@ -7,23 +7,23 @@ void draw() {
 	int nrows = 15;
 	int ncols = 60;
 	int x, y;
-	std::cout << "|";
+	std::cout << "#";
 	for (x = 0; x < ncols; x++) {
-		std::cout << "-";
+		std::cout << "#";
 	}
-	std::cout << "|" << std::endl;
+	std::cout << "#" << std::endl;
 	for (x = 0; x < nrows; x++) {
-		std::cout << "|";
+		std::cout << "#";
 		for (y = 0; y < ncols; y++) {
 			std::cout << " ";
 		}
-		std::cout << "|" << std::endl;
+		std::cout << "#" << std::endl;
 	}
-	std::cout << "|";
+	std::cout << "#";
 	for (x = 0; x < ncols; x++) {
-		std::cout << "-";
+		std::cout << "#";
 	}
-	std::cout << "|" << std::endl;
+	std::cout << "#" << std::endl;
 }
 
 
